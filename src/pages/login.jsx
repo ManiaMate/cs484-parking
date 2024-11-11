c027a65268250d3f41303539b63b543fcde2be97
Andrew76xx <amei01@calpoly.edu>
2024-11-10 17:12:35 -0800

    Completed Login Page

---
+++ src/pages/login.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./../styles/login.css";
+import logo from "./../Cal-Poly-Logo.png";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameFocus = () => {
+    if (username === "") {
+      setUsername("example@calpoly.edu");
+    }
+  };
+
+  const handlePasswordFocus = () => {
+    if (password === "") {
+      setPassword("fillerpassword");
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-header">
+          <img src={logo} alt="Poly Parking Logo" className="logo" />
+          Welcome To Poly Parking
+        </div>
+        <div className="login-title">Login to your Cal Poly Account</div>
+        <div className="login-form">
+          <div className="input">
+            <div className="label-container">
+              <label>Username:</label>
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                value={username}
+                onFocus={handleUsernameFocus}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="username@calpoly.edu"
+              />
+            </div>
+          </div>
+          <div className="input">
+            <div className="label-container">
+              <label>Password:&nbsp;</label>
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                value={password}
+                onFocus={handlePasswordFocus}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="submit-button">
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
