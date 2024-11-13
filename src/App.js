53eb4c93e463414d51bedf30fecb79c38c695f6f
Andrew76xx <amei01@calpoly.edu>
2024-11-12 21:08:38 -0800

    Completed History Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Homepage from "./pages/homepage";
+import HistoryPage from "./pages/history";
 
 
 import "./App.css";
@@ -10,7 +11,8 @@ function App() {
   return (  
     <div className="App">
       {/* <Login /> */}
-      <Homepage />
+      {/* <Homepage /> */}
+      <HistoryPage/>
     </div>
   );
 }
